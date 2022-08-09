f868084082e5c332cf60bbffb7e74d11b500b753
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-08-09 15:15:59 +0100

    ideas for posts schema added

--- models/ideas.js
+++ models/ideas.js
@@ -1,4 +1,6 @@
-const User = require("./user");
+const { isPromiseLike } = require("mongodb/lib/utils")
+
+USERS
 
     var Schema = mongoose.Schema
     const UserSchema = new Schema({
@@ -9,27 +11,52 @@ const User = require("./user");
       password: encrypted
       phoneNumber: optional
       image: (placeholder)
-      friends: [{ type: Schema.Types.ObjectId,  ref: 'Friends'}] //search in recipient & requester columns
       {timestamps: true}
+    
+      module.exports = mongoose.model('Users', usersSchema)
+      
+      const friendsSchema = new Schema({
+       FK  requester: { type: Schema.Types.ObjectId, ref: 'Users'},
+       FK  recipient: { type: Schema.Types.ObjectId, ref: 'Users'},
+          status: {
+              type: Number,
+              enums: [
+                  0,    //'pending',
+                  1,    //'friends'
+                ]
+            }
+        }, {timestamps: true})
+        
+        //for queries  
+    friends: [{ type: Schema.Types.ObjectId,  ref: 'Friends'}] //search in recipient & requester columns
 
-    
-    module.exports = mongoose.model('Users', usersSchema)
-    
-    const friendsSchema = new Schema({
-        requester: { type: Schema.Types.ObjectId, ref: 'Users'},
-        recipient: { type: Schema.Types.ObjectId, ref: 'Users'},
-        status: {
-            type: Number,
-            enums: [
-                0,    //'pending',
-                1,    //'friends'
-            ]
-        }
-    }, {timestamps: true})
-    
-  //for queries  
     friends_requests: [{ type: Schema.Types.ObjectId, ref: 'pending'}] //search in recipient column 
   }, {timestamps: true})
 
     friends_requestsThatIMade: [{ type: Schema.Types.ObjectId , ref: 'pending'}] //search in requester column 
   }, {timestamps: true})
+
+POSTS
+
+Schema 1
+post {
+    postId - ser
+    userID
+    message: str
+    {timestamps: true}
+}
+
+Schema 2
+comments {
+    commentId - ser 
+    postId - FK
+    userID - FK
+    content
+    {timestamps: true}
+    
+}
+Schema 3
+Likes
+    likeId ser
+    postId - FK
+    userID - FK
