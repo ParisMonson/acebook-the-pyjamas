b5160fbe96825ddfe65a4d9e4d2914b4fb784845
Shaun Ho <32503884+shaunywho@users.noreply.github.com>
2022-08-09 16:40:23 +0100

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -6,9 +6,6 @@ const UsersController = {
   },
 
   Create: (req, res) => {
-    console.log("res", res);
-    console.log("req", req);
-    console.log("req body", req.body);
     const user = new User(req.body);
     user.save((err) => {
       if (err) {
