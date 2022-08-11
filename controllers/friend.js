12f8cc209d271473c3a240c39e358ebbe73028b1
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-08-11 15:02:12 +0100

    friends controller v1'

---
+++ controllers/friend.js
@@ -0,0 +1,50 @@
+const Friend = require("../models/friend");
+const User = require("../models/user");
+
+
+const FriendsController = {
+//   Profile: (req, res) => {
+//     Friend.findOne({ username: req.params.username }, (err, user) => {
+//       if (err) {
+//         throw err;
+//       }
+//       res.render("users/profile", {
+//         user: user,
+//         session: req.session,
+//       });
+//     });
+//   },
+
+  // a path to create a request - should be on click - which page
+  // to change the path later
+
+  New: (req, res) => {
+    res.render("friends/new", {});  
+  },
+
+ 
+  Create: (req, res) => {
+    const friendRequest = new Friend(req.body);
+    friendRequest.save((err) => {
+      if (err) {
+        throw err;
+      }
+      res.status(201).redirect("/posts"); /// ???
+    });
+  },
+
+  //delete from friends once confirmed
+
+// ?db.removeUser(username)
+  Create: (req, res) => {
+    const friendRequest = new Friend(req.body);
+    friendRequest.save((err) => {
+      if (err) {
+        throw err;
+      }
+      res.status(201).redirect("/posts"); /// ???
+    });
+  },
+};
+
+module.exports = FriendsController;
